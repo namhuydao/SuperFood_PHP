dc8fa198ced717fa58df10cbb7c7f0c6ce52ef06
namhuydao <namhuydao@tutamail.com>
2021-03-10 09:41:30 +0700

    add changes

--- database/database.php
+++ database/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "superfood";
+$dbname = "superfoodphp";
 $conn = new mysqli($servername, $username, $password, $dbname);
 mysqli_set_charset($conn, 'utf8');
 
